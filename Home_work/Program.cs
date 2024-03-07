a520d48b0ccf74e5deb1f0481791787539756542
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 03:21:14 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            // Split the sentence into words based on spaces
+            string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Count the number of words
+            int wordCount = words.Length;
+
+            Console.WriteLine($"The sentence contains {wordCount} {(wordCount == 1 ? "word" : "words")}.");
+        }
+    }
+}
