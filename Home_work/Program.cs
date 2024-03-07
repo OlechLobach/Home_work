f39264fc89a99096309922a3acec7fd7875f8a68
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 03:27:37 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            int vowelCount = CountVowels(sentence);
+
+            Console.WriteLine($"Number of vowels in the sentence: {vowelCount}");
+        }
+
+        static int CountVowels(string sentence)
+        {
+            int count = 0;
+            foreach (char c in sentence)
+            {
+                if (IsVowel(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static bool IsVowel(char c)
+        {
+            c = char.ToLower(c);
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+    }
+}
