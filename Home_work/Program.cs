cd0949e1a57c6713ebf0a06ea956bad5ebb6f31c
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 16:05:59 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a text:");
+            string text = Console.ReadLine();
+
+            string result = CapitalizeFirstLetter(text);
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(result);
+        }
+
+        static string CapitalizeFirstLetter(string text)
+        {
+            char[] chars = text.ToCharArray();
+            bool capitalizeNext = true;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (capitalizeNext && char.IsLetter(chars[i]))
+                {
+                    chars[i] = char.ToUpper(chars[i]);
+                    capitalizeNext = false;
+                }
+                else if (chars[i] == '.' || chars[i] == '!' || chars[i] == '?')
+                {
+                    capitalizeNext = true;
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}
