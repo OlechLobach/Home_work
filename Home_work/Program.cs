807e10c8cc4d8e89f747a18c4da3106745ac3715
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 15:53:17 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,98 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+    class Home_work
+    {
+        static void Main()
+        {
+
+            int[,] matrix1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] matrix2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+
+            Console.WriteLine("Matrix 1:");
+            PrintMatrix(matrix1);
+
+            Console.WriteLine("\nMatrix 2:");
+            PrintMatrix(matrix2);
+
+
+            Console.WriteLine("\nScalar multiplication of Matrix 1 by 2:");
+            int[,] scalarProduct = ScalarMultiply(matrix1, 2);
+            PrintMatrix(scalarProduct);
+
+
+            Console.WriteLine("\nMatrix addition:");
+            int[,] sum = AddMatrices(matrix1, matrix2);
+            PrintMatrix(sum);
+
+            Console.WriteLine("\nMatrix multiplication:");
+            int[,] product = MultiplyMatrices(matrix1, matrix2);
+            PrintMatrix(product);
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int[,] ScalarMultiply(int[,] matrix, int scalar)
+        {
+            int[,] result = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    result[i, j] = matrix[i, j] * scalar;
+                }
+            }
+            return result;
+        }
+
+        static int[,] AddMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            {
+                throw new ArgumentException("Matrices must have the same dimensions.");
+            }
+
+            int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return result;
+        }
+
+        static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            {
+                throw new ArgumentException("Number of columns in Matrix 1 must be equal to number of rows in Matrix 2.");
+            }
+
+            int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                        result[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
