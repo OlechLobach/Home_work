d579e5df3fe07c84da6ad79167aa111153fee09b
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 16:12:07 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the text:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Enter the forbidden word:");
+            string forbiddenWord = Console.ReadLine();
+
+            int replacementsCount = 0;
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Equals(forbiddenWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    words[i] = new string('*', words[i].Length);
+                    replacementsCount++;
+                }
+            }
+
+            string result = string.Join(" ", words);
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(result);
+            Console.WriteLine($"Statistics: {replacementsCount} replacements of the word \"{forbiddenWord}\".");
+        }
+    }
+}
