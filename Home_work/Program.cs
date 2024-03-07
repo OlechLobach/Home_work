5916e8af0e7031509c307338d24ab5d780e9130d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 03:25:00 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string reversedSentence = ReverseWordsInSentence(words);
+
+            Console.WriteLine("Reversed sentence:");
+            Console.WriteLine(reversedSentence);
+        }
+
+        static string ReverseWord(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static string ReverseWordsInSentence(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = ReverseWord(words[i]);
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
