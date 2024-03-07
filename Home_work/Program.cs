ab8198761f61f1ebca9d8a100adb75e8479710a9
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-07 03:30:14 +0200

    initiall commit

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+namespace Home_work
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("Enter a substring to search for:");
+            string substring = Console.ReadLine();
+
+            int count = CountSubstringOccurrences(sentence, substring);
+
+            Console.WriteLine($"Number of occurrences of '{substring}' in the sentence: {count}");
+        }
+
+        static int CountSubstringOccurrences(string sentence, string substring)
+        {
+            int count = 0;
+            int index = 0;
+            while ((index = sentence.IndexOf(substring, index)) != -1)
+            {
+                count++;
+                index += substring.Length;
+            }
+            return count;
+        }
+    }
+}
